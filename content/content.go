6b287a18f42fddd1c35a0c63b77def4a6654fc08
ZhaoQaoWang <873908960@qq.com>
2018-10-23 18:30:00 +0800

    1.CONTENT 测试

--- content/content.go
+++ content/content.go
@@ -1,29 +1,93 @@
-package content
+package main
 
 import (
+	"time"
+	"context"
 	"fmt"
-	"math"
 )
 
+var key string="name"
+var keys string="names"
+
 func main() {
-	fmt.Println(int64(math.Ceil((float64(42)/float64(41)))))
+	ctx, cancel := context.WithCancel(context.Background())
+	//附加值
+	valueCtx:=context.WithValue(ctx,key,"【监控1】")
+	go watch(valueCtx)
+	aa := context.WithValue(valueCtx,keys,"【监控3】")
+	go watch(aa)
+	time.Sleep(10 * time.Second)
+	fmt.Println("可以了，通知监控停止")
+	cancel()
+	//为了检测监控过是否停止，如果没有监控输出，就表示停止了
+	time.Sleep(5 * time.Second)
+}
+
+func watch(ctx context.Context) {
+	for {
+		select {
+		case <-ctx.Done():
+			//取出值
+			fmt.Println(ctx.Value(key),"监控退出，停止了...")
+			return
+		default:
+			//取出值
+			fmt.Println(ctx.Value(key),"goroutine监控中...")
+			time.Sleep(2 * time.Second)
+		}
+	}
 }
 
 
-//tick := time.Tick(time.Second)
-//for count :=10 ; count > 0; count-- {
-//fmt.Println(count)
-//fmt.Println(tick)
+//func main() {
+//	ctx, cancel := context.WithCancel(context.Background())
+//	go watch(ctx,"【监控1】")
+//	go watch(ctx,"【监控2】")
+//	go watch(ctx,"【监控3】")
+//
+//	time.Sleep(10 * time.Second)
+//	fmt.Println("可以了，通知监控停止")
+//	cancel()
+//	fmt.Println(1111)
+//	//为了检测监控过是否停止，如果没有监控输出，就表示停止了
+//}
+//
+//func watch(ctx context.Context, name string) {
+//	for {
+//		select {
+//		case <-ctx.Done():
+//			fmt.Println(name,"监控退出，停止了...")
+//			f,_ := os.Create(name+".txt")
+//			f.Write([]byte(name))
+//			f.Close()
+//			return
+//		default:
+//			fmt.Println(name,"goroutine监控中...")
+//			time.Sleep(2 * time.Second)
+//		}
+//	}
 //}
 
-
-//select {
-//case <-ch1:
-//fmt.Println("ch1 ch1")
-//case x := <-ch2:
-//fmt.Printf("default %s\n ",x)
-//case ch3 <- y:
-//fmt.Println("ch3 ch3")
-//default:
-//fmt.Printf("default %s\n ","1234")
+//
+//func main() {
+//	ctx, cancel := context.WithCancel(context.Background())
+//	go func(ctx context.Context) {
+//		for {
+//			select {
+//			case <-ctx.Done():
+//				fmt.Println("监控退出，停止了...")
+//				return
+//			default:
+//				fmt.Println("goroutine监控中...")
+//				time.Sleep(2 * time.Second)
+//			}
+//		}
+//	}(ctx)
+//
+//	time.Sleep(10 * time.Second)
+//	fmt.Println("可以了，通知监控停止")
+//	cancel()
+//	//为了检测监控过是否停止，如果没有监控输出，就表示停止了
+//	time.Sleep(5 * time.Second)
+//
 //}
