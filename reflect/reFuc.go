cccb062f9459b060d12f2c51ee9d9150cba0dd5f
ZRothschild <893908960@qq.com>
2019-03-24 13:12:30 +0800

    1. 反射包学习

---
+++ reflect/reFuc.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func main() {
+	//函数值
+	//test := test
+	//fmt.Printf("%T %v\n",test,test)
+	pStr := "你好北京"
+	//test(pStr)
+
+	//判断 test 类型
+	fv := reflect.ValueOf(test)
+	//fv.Kind() 判断是否为函数，否则报错
+	fmt.Println("fv is reflect.Func ?", fv.Kind() == reflect.Func)
+	//生成reflect.Value 组装数据
+	intParams := make([]reflect.Value, 0)
+	intParams = append(intParams, reflect.ValueOf(pStr))
+	//注意参数个数与函数接收参数保持一致
+	//intParams := make([]reflect.Value,1)
+	//intParams[0] = intParams,reflect.ValueOf(pStr)
+	fvRe := fv.Call(intParams)
+	fmt.Printf("没有返回值 %T\n", fvRe) //[]reflect.Value
+
+	/**************************************************************************************/
+
+	//多参数传值
+	testMany := reflect.ValueOf(testMany)
+	if testMany.Kind() == reflect.Func {
+		intMany := make([]reflect.Value, 0)
+		intMany = append(intMany, reflect.ValueOf(pStr), reflect.ValueOf(pStr), reflect.ValueOf(pStr))
+		testRes := testMany.Call(intMany)
+		//Interface 变成接口类型
+		fmt.Printf("返回值 %T %T %T\n",
+			testRes,
+			testRes[0].Interface(),
+			testRes[0].Interface().(string))
+	}
+}
+
+func test(str string) {
+	var s string = "单个参数"
+	s = s + str
+	fmt.Println(s)
+}
+
+func testMany(str ...string) string {
+	var s string = "多参数"
+	//str []string
+	fmt.Printf("%T %v\n", str, str)
+	for _, v := range str {
+		s = s + v
+	}
+	fmt.Println(s)
+	return s
+}
---
+++ reflect/reMeth.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+	"strconv"
+)
+
+func main() {
+
+	//var i int = 2
+	////Elem 返回值
+	//inte := reflect.ValueOf(&i).Elem()
+	//fmt.Printf("%v,%T\n",inte,inte)     //2，ptr
+
+	myType := &MyType{22, "my name is liLei"}
+	//reflect.ValueOf 返回地址
+	//reflect.ValueOf(&myType).Elem() 返回值  &{22 my name is liLei}
+	mtV := reflect.ValueOf(&myType).Elem()
+	//触发 String 方法   0xc000052400--name:my name is liLei i:22  =====  reflect.Value
+	fmt.Printf("%v  =====  %T\n", mtV, mtV)
+	//因为打印是有返回值所有去第零个返回值
+	fmt.Println("Before:", mtV.MethodByName("String").Call(nil)[0])
+
+	params := make([]reflect.Value, 1)
+	params[0] = reflect.ValueOf(18)
+	mtV.MethodByName("SetI").Call(params)
+	params[0] = reflect.ValueOf("reflection test")
+	mtV.MethodByName("SetName").Call(params)
+	fmt.Println("After:", mtV.MethodByName("String").Call(nil)[0])
+	fmt.Println("After:", mtV.Method(2).Call(nil)[0])
+}
+
+type MyType struct {
+	i    int
+	name string
+}
+
+func (mt *MyType) SetI(i int) {
+	mt.i = i
+}
+
+func (mt *MyType) SetName(name string) {
+	mt.name = name
+}
+
+func (mt *MyType) String() string {
+	return fmt.Sprintf("%p", mt) + "--name:" + mt.name + " i:" + strconv.Itoa(mt.i)
+}
