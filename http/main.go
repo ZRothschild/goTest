79895401701629309edc6ef075d76a8c41b61183
ZRothschild <873908960@qq.com>
2019-08-26 00:18:21 +0800

    '测试json'

--- http/main.go
+++ http/main.go
@@ -1,19 +1,29 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
+
+type Data struct {
+	Email string `json:"email"`
+	Password string `json:"password"`
+	Captcha string `json:"captcha"`
+	VerifyString string `json:"verify_string"`
+}
 
 func main() {
 	db := database{"shoes": 50, "socks": 5}
 	serveMux := http.NewServeMux()
 	serveMux.HandleFunc("/list", db.list)
 	serveMux.HandleFunc("/price", db.price)
+	serveMux.HandleFunc("/test", db.test)
 	hand := http.StripPrefix("/tmp", http.FileServer(http.Dir("F:\\goWork\\goTest\\http")))
 	serveMux.Handle("/tmp", hand)
-	server := &http.Server{Addr: "localhost:80", Handler: serveMux}
+	server := &http.Server{Addr: "localhost:8080", Handler: serveMux}
 	log.Fatal(server.ListenAndServe())
 }
 
@@ -44,6 +54,40 @@ func (db database) price(w http.ResponseWriter, req *http.Request) {
 		return
 	}
 	fmt.Fprintf(w, "%f\n", price)
+}
+
+//测试json
+func (db database) test (w http.ResponseWriter, req *http.Request) {
+	ioReadCloser := req.Body
+	fmt.Printf("ioReadCloser %v\n",ioReadCloser)
+
+	tetIo := ioReadCloser
+	//读取了 ioReadCloser 就会为空 可以看 req 变化就可知
+	reqBody,err := ioutil.ReadAll(ioReadCloser)
+	if err != nil {
+		fmt.Errorf("%s\n",err)
+	}
+	var reqData Data
+	err = json.Unmarshal(reqBody,&reqData)
+	if err != nil {
+		fmt.Printf("reqData => %s\n",err)
+	}
+	fmt.Printf("%v\n",reqData)
+
+	var mapData map[string]string
+	err = json.Unmarshal(reqBody,&mapData)
+	if err != nil {
+		fmt.Errorf("mapData %s\n",err)
+	}
+	fmt.Printf("%v\n",mapData)
+
+	var reqDataDe Data
+	//是取地址改变了  ioReadCloser buff 已经被读取过了
+	json.NewDecoder(tetIo).Decode(&reqDataDe)
+	fmt.Printf("reqDataDe => %v\n",reqDataDe)
+
+	reqStr := string(reqBody)
+	fmt.Printf("%s\n",reqStr)
 }
 
 // mux := http.NewServeMux()
