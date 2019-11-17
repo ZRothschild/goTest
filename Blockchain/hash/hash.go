e2d945f001e528b8cb330d7909b7360ab64e976d
ZRothschild <873908960@qq.com>
2019-11-18 00:06:01 +0800

    1.练习

--- Blockchain/hash/hash.go
+++ Blockchain/hash/hash.go
@@ -22,6 +22,32 @@ import (
 // h.Reset()//重要！！！
 // h.Write([]byte("string2"))
 // fmt.Printf("%x\n", h.Sum(nil))
+// hex.DecodeString 字符串转十六进制 字符串与十六进制加密产生的结果不一样，数字货币用十六进制
+/**
+package main
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"fmt"
+)
+
+func main() {
+	sha1New := md5.New()
+	src := "123456";
+	srcB,_ := hex.DecodeString(src)
+	fmt.Printf("123456 r => %x\n", srcB)
+	b := []byte("123456")
+	sha1New.Write(b)
+	nSum := sha1New.Sum(nil)
+	fmt.Printf("123456 r => %x\n", nSum)
+	sha1New.Reset()
+	sha1New.Write(srcB)
+	nSum = sha1New.Sum(nil)
+	fmt.Printf("123 r => %x\n", nSum)
+}
+
+*/
 
 func Sha1Bytes(str string, pre []byte) string {
 	sha1New := sha1.New()
--- Blockchain/main.go
+++ Blockchain/main.go
@@ -2,14 +2,21 @@ package main
 
 import (
 	"crypto/md5"
+	"encoding/hex"
 	"fmt"
 )
 
 func main() {
 	sha1New := md5.New()
+	src := "123456"
+	srcB, _ := hex.DecodeString(src)
+	fmt.Printf("123456 r => %x\n", srcB)
 	b := []byte("123456")
-	// a := []byte("456")
 	sha1New.Write(b)
-	nSum := sha1New.Sum(b)
-	fmt.Printf("123456 r => %x", nSum)
+	nSum := sha1New.Sum(nil)
+	fmt.Printf("123456 r => %x\n", nSum)
+	sha1New.Reset()
+	sha1New.Write(srcB)
+	nSum = sha1New.Sum(nil)
+	fmt.Printf("123 r => %x\n", nSum)
 }
